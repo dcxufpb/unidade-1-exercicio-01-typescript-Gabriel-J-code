45fcc539812d706c14cd702b46a4506f0dd74d0b
Gabriel de Jesus de <gabriel.franca@dce.ufpb.br>
2020-09-16 17:29:22 +0000

    Método 'imprime_dados_loja()' implementado.

--- cupom.ts
+++ cupom.ts
@@ -1,6 +1,27 @@
+let dados = {
+  nome_loja: "Arcos Dourados Com. de Alimentos LTDA",
+  logradouro: "Av. Projetada Leste",
+  numero: 500,
+  complemento: "EUC F32/33/34",
+  bairro: "Br. Sta Genebra",
+  municipio: "Campinas",
+  estado: "SP",
+  cep: "13080-395",
+  telefone: "(19) 3756-7408",
+  observacao: "Loja 1317 (PDP)",
+  cnpj: "42.591.651/0797-34",
+  inscricao_estadual: "244.898.500.113"
+};
+
 function imprime_dados_loja() {
-  // Implemente aqui
-  return "";
+	var aux: string = dados.nome_loja + "\n"+
+	dados.logradouro + ", " + dados.numero.toString() + " " + dados.complemento +"\n"+
+	dados.bairro + " - " + dados.municipio + " - " + dados.estado + "\n" +
+	"CEP:" + dados.cep + " Tel " + dados.telefone + "\n" +
+	dados.observacao + "\n" +
+	"CNPJ: " + dados.cnpj + "\n" +
+	"IE: " + dados.inscricao_estadual + "\n"
+	return aux;
 }
 
 module.exports = imprime_dados_loja;
